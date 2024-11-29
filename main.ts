139121363516e08890cb9a256b3de4f90f56368a
박성현 <silkcoop@gmail.com>
2024-11-30 02:50:36 +0900

    [Chore] Clean up project structure and naming
    - Update project naming
      - Change plugin name to 'Arxiv Assistant'
      - Update package name and metadata
      - Update plugin description
    
    - Remove unnecessary code
      - Remove sample plugin commands
      - Remove unused modal components
      - Clean up redundant functions
    
    - Keep core features only
      - PDF text extraction
      - Arxiv metadata fetching
      - PDF downloading
      - Paper summarization

--- main.ts
+++ main.ts
@@ -1,11 +1,9 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
+import { App, Notice, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
 import { PDFExtractor } from './src/services/pdfExtractor';
 import { ArxivMetadataService } from './src/services/arxivMetadataService';
 import { PDFDownloadService } from './src/services/pdfDownloadService';
 import { PluginSettings, DEFAULT_SETTINGS } from './src/types/settings';
 import { SummaryService } from './src/services/summaryService';
-
-// Remember to rename these classes and interfaces!
 
 export default class ArxivAssistantPlugin extends Plugin {
 	settings: PluginSettings;
@@ -21,66 +19,13 @@ export default class ArxivAssistantPlugin extends Plugin {
 		this.pdfDownloadService = new PDFDownloadService(this.app, this.settings);
 		this.summaryService = new SummaryService(this.app, this.settings);
 
-		// This creates an icon in the left ribbon.
+		// 리본 아이콘 추가
 		const ribbonIconEl = this.addRibbonIcon('book', 'Arxiv Assistant', (evt: MouseEvent) => {
 			new Notice('Arxiv Assistant is ready!');
 		});
 		ribbonIconEl.addClass('arxiv-assistant-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new ArxivAssistantSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-
-		// PDF 텍스트 추출 명령어 수정
+		// PDF 텍스트 추출 명령어
 		this.addCommand({
 			id: 'get-text-from-pdf',
 			name: 'Get Text From PDF',
@@ -94,28 +39,20 @@ export default class ArxivAssistantPlugin extends Plugin {
 						return;
 					}
 
-					// TFile 타입 체크
 					if (file instanceof TFile) {
 						const arrayBuffer = await this.app.vault.readBinary(file);
 						const extractedText = await this.pdfExtractor.extractTextFromPDF(arrayBuffer);
 						
-						// 새로운 마크다운 파일 이름
 						const newFileName = '2404.16260v1-extracted.md';
-						
-						// 기존 파일 존재 여부 확인
 						const existingFile = this.app.vault.getAbstractFileByPath(newFileName);
+						
 						if (existingFile) {
-							// 기존 파일이 있으면 수정
 							await this.app.vault.modify(existingFile as TFile, extractedText);
 							new Notice('기존 파일이 업데이트되었습니다.');
 						} else {
-							// 새 파일 생성
 							await this.app.vault.create(newFileName, extractedText);
 							new Notice('새 파일이 생성되었습니다.');
 						}
-						
-						// 디버깅용 로그
-						console.log('추출된 텍스트:', extractedText);
 					} else {
 						new Notice('유효하지 않은 파일입니다.');
 					}
@@ -126,7 +63,7 @@ export default class ArxivAssistantPlugin extends Plugin {
 			}
 		});
 
-		// Arxiv 메타데이터 명령어 추가
+		// Arxiv 메타데이터 명령어
 		this.addCommand({
 			id: 'fetch-arxiv-metadata',
 			name: 'Arxiv 메타데이터 가져오기',
@@ -135,7 +72,7 @@ export default class ArxivAssistantPlugin extends Plugin {
 			}
 		});
 
-		// PDF 다운로드 명령어 추가
+		// PDF 다운로드 명령어
 		this.addCommand({
 			id: 'download-arxiv-pdf',
 			name: 'Arxiv PDF 다운로드',
@@ -144,7 +81,7 @@ export default class ArxivAssistantPlugin extends Plugin {
 			}
 		});
 
-		// 요약 명령어 추가
+		// 요약 명령어
 		this.addCommand({
 			id: 'summarize-arxiv-paper',
 			name: 'Arxiv 논문 요약하기',
@@ -152,10 +89,12 @@ export default class ArxivAssistantPlugin extends Plugin {
 				this.summaryService.summarizeFromClipboard();
 			}
 		});
+
+		// 설정 탭 추가
+		this.addSettingTab(new ArxivAssistantSettingTab(this.app, this));
 	}
 
 	onunload() {
-
 	}
 
 	async loadSettings() {
@@ -164,22 +103,6 @@ export default class ArxivAssistantPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
 	}
 }
 
@@ -193,7 +116,6 @@ class ArxivAssistantSettingTab extends PluginSettingTab {
 
 	display(): void {
 		const {containerEl} = this;
-
 		containerEl.empty();
 
 		new Setting(containerEl)
